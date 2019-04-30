b12d48460fa859d5df92c54e6922173f31ddcf6f
Colin Sandlin <csandlin010@gmail.com>
2019-04-30 12:34:22 -0500

    added functions to add junk to the drawer, and a dynamically loading categories dropdown menu

--- src/scripts/dom-junk.js
+++ src/scripts/dom-junk.js
@@ -1,21 +1,20 @@
-const junkDom = (obj) => {
-    return `<div>
+const junkDom = obj => {
+  return `<div>
                 ${obj.name}
                 ${obj.categoryId}
             </div>`;
 };
 
-
 function buildJunkDom(dataArray) {
-    mainContainer.innerHTML = "";
-    dataArray.forEach((obj) => {
-        mainContainer.innerHTML += junkDom(obj);
-    });
-};
+  mainContainer.innerHTML = "";
+  dataArray.forEach(obj => {
+    mainContainer.innerHTML += junkDom(obj);
+  });
+}
 
-const buildJunkDetailDom = (data) => {
-    mainContainer.innerHTML = "";
-    data.forEach(element => {
-        mainContainer.innerHTML += junkDetailDom(element);
-    });
+const buildJunkDetailDom = data => {
+  mainContainer.innerHTML = "";
+  data.forEach(element => {
+    mainContainer.innerHTML += junkDetailDom(element);
+  });
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,52 +1,75 @@
+//COLIN
+const mainContainer = document.querySelector("#container");
+
+const populateCategoryDropdown = () => {
+  API.populateCategories().then(result => {
+    result.forEach(object => {
+      console.log(object.name);
+      const targetDropdown = document.querySelector("#selectMenu");
+      console.log(targetDropdown);
+      targetDropdown.innerHTML += `<option value="${object.id}">${
+        object.name
+      } </option>`;
+    });
+  });
+};
+populateCategoryDropdown();
 
 handleGetJunkClick = () => {
-    API.getJunk()
-    .then(result => {
-        console.log("junk", result);
-        buildJunkDom(result);
-    });
+  API.getJunk().then(result => {
+    console.log("junk", result);
+    buildJunkDom(result);
+  });
 };
 
 handleGetCategoryClick = () => {
-    API.getCategories()
-        .then(result => {
-            console.log("category", result);
-            buildCategoryDom(result);
-        });
+  API.getCategories().then(result => {
+    console.log("category", result);
+    buildCategoryDom(result);
+  });
 };
 
 handleGetCategoryDetailClick = () => {
-    API.getCategoryWithJunk()
-        .then(result => {
-            console.log("category list", result);
-            buildCategoryDetailDom(result);
-        });
+  API.getCategoryWithJunk().then(result => {
+    console.log("category list", result);
+    buildCategoryDetailDom(result);
+  });
 };
 
 document.querySelector("#btn-saveCategory").addEventListener("click", event => {
-    const category = document.querySelector(".junk__category").value;
+  const category = document.querySelector(".junk__category").value;
 
-        // Once you have collected all the values, build your data structure
-        const categoryObj = {
-            name: category
-        };
+  // Once you have collected all the values, build your data structure
+  const categoryObj = {
+    name: category
+  };
 
-    API.saveCategory(categoryObj)
-    .then(parsedResult => {
-        console.log("what is the result", parsedResult);
-    });
-
+  API.saveCategory(categoryObj).then(parsedResult => {
+    console.log("what is the result", parsedResult);
+  });
 });
 
+//COLIN
+document.querySelector("#btn-saveJunk").addEventListener("click", event => {
+  const newItem = document.querySelector("#junk__add").value;
+  const newItemCategoryId = parseInt(
+    document.querySelector("#selectMenu").value
+  );
+  const newJunkObj = {
+    name: newItem,
+    categoryId: newItemCategoryId
+  };
+  API.saveNewJunkItem(newJunkObj).then(parsedResult => {
+    console.log("what is the new item", parsedResult);
+  });
+});
 
-document.querySelector("#btn-getJunk").addEventListener("click", handleGetJunkClick);
-document.querySelector("#btn-getCategories").addEventListener("click", handleGetCategoryClick);
-document.querySelector("#btn-getCategoryDetails").addEventListener("click", handleGetCategoryDetailClick);
-
-const mainContainer = document.querySelector("#container");
-
-
-
-
-
-
+document
+  .querySelector("#btn-getJunk")
+  .addEventListener("click", handleGetJunkClick);
+document
+  .querySelector("#btn-getCategories")
+  .addEventListener("click", handleGetCategoryClick);
+document
+  .querySelector("#btn-getCategoryDetails")
+  .addEventListener("click", handleGetCategoryDetailClick);
